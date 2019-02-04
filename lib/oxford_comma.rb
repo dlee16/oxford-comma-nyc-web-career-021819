f744af8a209ff72116119d3c53b8bce520172205
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 16:37:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,9 @@ if array.length ==1
   return array.join
 elsif array.length ==2 
 return array.join(" and ")
-elsif array.length == 3
-return 
+else
+array.length >= 3
+return array(1..-2).joins
 else 
   return array(1..-2).joins(", ") + "and" + array.last
 end
