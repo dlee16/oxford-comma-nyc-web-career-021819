b49988d87cfd7e94654702c4501b790d2bb1c049
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 16:44:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
 if array.length ==1 
   return array.join
-  elsif array.length ==2 
-return array.join(" and ")
-else
-array.length >= 3
-return array[0..-2].join(", ") + ", and " + array.last
+elsif array.length ==2 
+  return array.join(" and ")
+else array.length >= 3
+  return array[0..-2].join(", ") + ", and " + array.last
 end
 end
